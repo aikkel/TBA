ab41ba79858da288a5458cce6a5b8394e69302d8
aikkel <mikk39k8@edu.zealand.dk>
2024-04-01 10:16:13 +0200

    sound not working correctly

--- Media/Sound.py
+++ Media/Sound.py
@@ -15,16 +15,27 @@ class SoundPlayer:
         print(sound_path)
         winsound.PlaySound(sound_path, winsound.SND_FILENAME)
 
+    def play_whoosh(self):
+        self.play_sound('whoosh.wav')
+
+    def play_hero_attack(self):
+        self.play_sound('heroattack.wav')
+
+    def play_orc_attack(self):
+        self.play_sound('orcattack.wav')
+
+    def play_lucky(self):
+        self.play_sound('lucky.wav')
+
+    def play_unlucky(self):
+        self.play_sound('unlucky.wav')
+
 # Create a SoundPlayer instance
 player = SoundPlayer()
 
-# Play the sound files
-player.play_sound('whoosh.wav')
-sleep(1)
-player.play_sound('heroattack.wav')
-sleep(1)
-player.play_sound('orcattack.wav')
-sleep(1)
-player.play_sound('lucky.wav')
-sleep(1)
-player.play_sound('unlucky.wav')
+# Play the specific sound files
+# player.play_lucky()
+# sleep(1)
+# player.play_unlucky()
+# sleep(1)
+# player.play_hero_attack()
